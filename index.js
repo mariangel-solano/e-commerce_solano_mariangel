39be3865a92a6c6ba5f1fb18e673371795273e63
Mari <mafisd@gmail.com>
2024-08-29 18:45:27 -0500

    modifiqué con querySelector

---
+++ index.js
@@ -0,0 +1,5 @@
+const name= "Carpaccio de higos";
+const img= "https://laspiedras.cl/wp-content/uploads/2021/05/Captura-de-Pantalla-2021-05-31-a-las-13.09.15.png";
+
+document.querySelector ("p").innerText= name;
+document.querySelector ("img").src= img;

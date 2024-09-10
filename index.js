1325c757523b9519d35d4c5d9ed4b75238816ffe
Mari <mafisd@gmail.com>
2024-09-10 18:03:07 -0500

    Ejercicio clase 8

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-document.querySelector ("h1").innerText= "Productos";
-array= [];
-for (const i= 1 ; i <= 9 ; i++) {
-    const cards= 
- `<div class="card">
-<img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="">
-<p>Producto ${i}</p>
-</div>` ;
-array.push(cards);
-document.querySelector("section").innerHTML = (array);
+const h1= document.querySelector ("h1").innerText= "Productos";
+const section= document.querySelector ("section");
+let array= [];
+for (let i=1 ; i <= 9 ; i++) {
+    array.push (`<div class="card" style="width: 18rem;">
+  <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="">
+  <div class="card-body">
+    <h5 class="card-title">Car ${i}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`) ;
+section.innerHTML = array.join(``).replaceAll(",", "");
+document.querySelector(".container ").innerHTML = (array);
 }
 
 //const container= document.querySelector ("section.container");

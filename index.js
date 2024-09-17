47648aa9e281beed1d9d3c41629c501f9af1c512
Mari <mafisd@gmail.com>
2024-09-17 16:53:23 -0500

    Cambios clase 10

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const h1= document.querySelector ("h1").innerText= "Productos";
   const section= document.querySelector ("section");
   let array= [];
 
-function hola () {
+function homeDinamico () {
   for (let i=1 ; i <= 9 ; i++) {
       array.push (`<div class="card" style="width: 18rem;">
     <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="">
     <div class="card-body">
       <h5 class="card-title">Car ${i}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary">Ver más</a>
     </div>
   </div>`) ;
   }
   section.innerHTML = array.join(``).replaceAll(",", "");
 }
 
-hola ()
+homeDinamico ();

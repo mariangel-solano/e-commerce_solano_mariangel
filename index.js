3687d482ae6c927d4cd4d8bd75e04e739cbe4428
Mari <mafisd@gmail.com>
2024-09-19 18:33:22 -0500

    Agregue class

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function homeDinamico () {
     <div class="card-body">
       <h5 class="card-title">Car ${i}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Ver más</a>
+      <a href="producto.html" class="btn btn-primary">Ver más</a>
     </div>
   </div>`) ;
   }
---
+++ producto.js
@@ -0,0 +1,23 @@
+class Producto {
+    constructor (titulo, detalle, precio, stock, imagen) {
+        this.titulo= titulo;
+        this.detalle= detalle;
+        this.precio= precio;
+        this.stock= stock;
+        this.imagen= imagen;
+    }
+}
+
+const auto = new Producto ("Bmw m240i", "BMW Serie 2 3.0 M240i F22 Coupe", "$3600", 10, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg")
+let etiquetas=  ` <div>
+<h1> Descripción </h1>
+<h2> Titulo: ${auto.titulo}</h2>
+<p> Detalle: ${auto.detalle}</p>
+<p> Precio: ${auto.precio}</p>
+<p> Stock: ${auto.stock}</p>
+<img src="${auto.imagen}" alt="Imagen del auto widht="150" height="150" />
+</div>
+`
+let main= document.querySelector ("main");
+main.innerHTML= etiquetas;
+

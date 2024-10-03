260866514b7b1fc9920892313a4241f12170c7e6
Mari <mafisd@gmail.com>
2024-10-03 15:28:01 -0500

    ejercicio array (con Error)

--- index.js
+++ index.js
@@ -1,19 +1,99 @@
-const h1= document.querySelector ("h1").innerText= "Productos";
-  const section= document.querySelector ("section");
-  let array= [];
+const h1 = document.querySelector("h1");
 
-function homeDinamico () {
-  for (let i=1 ; i <= 9 ; i++) {
-      array.push (`<div class="card" style="width: 18rem;">
-    <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="">
-    <div class="card-body">
-      <h5 class="card-title">Car ${i}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="producto.html" class="btn btn-primary">Ver más</a>
-    </div>
-  </div>`) ;
-  }
-  section.innerHTML = array.join(``).replaceAll(",", "");
-}
+const section = document.querySelector("section");
 
-homeDinamico ();
+h1.innerText = "Productos";
+
+const autos = [
+    {
+    id: 1,
+    modelo: "Tundra",
+    descripcion: "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    precio: 34,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg`,
+  }, {
+    id: 2,
+    modelo: "I",
+    descripcion: "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    precio: 62,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/2.jpg`,
+  }, {
+    id: 3,
+    modelo: "M3",
+    descripcion: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    precio: 14,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/3.jpg`,
+  }, {
+    id: 4,
+    modelo: "Rio",
+    descripcion: "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    precio: 66,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/4.jpg`,
+  }, {
+    id: 5,
+    modelo: "Ram Van B250",
+    descripcion: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+    precio: 99,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/5.jpg`,
+  }, {
+    id: 6,
+    modelo: "Econoline E150",
+    descripcion: "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    precio: 23,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/6.jpg`,
+  }, {
+    id: 7,
+    modelo: "Rabbit",
+    descripcion: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    precio: 54,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/7.jpg`,
+  }, {
+    id: 8,
+    modelo: "928",
+    descripcion: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
+    precio: 63,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/8.jpg`,
+  }, {
+    id: 9,
+    modelo: "Intrepid",
+    descripcion: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+    precio: 52,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/9.jpg`,
+  }];
+
+
+const cards = autos.map((auto) =>
+                    `<div class="card" style="width: 18rem;">
+  <img src="${auto.url}" class="card-img-top" alt="">
+     <div class="card-body">
+       <h5 class="card-title">Car ${auto.modelo}</h5>
+       <p class="card-text">${auto.descripcion}</p>
+       <a href="/pages/producto.html?prod=${auto.id}" class="btn btn-primary">Ver más</a>
+     </div>
+   </div> `);
+
+section.innerHTML = cards.join().replaceAll(",", "");
+
+
+
+
+
+// const h1= document.querySelector ("h1").innerText= "Productos";
+//   const section= document.querySelector ("section");
+//   let array= [];
+
+// function homeDinamico () {
+//   for (let i=1 ; i <= 9 ; i++) {
+//       array.push (`<div class="card" style="width: 18rem;">
+//     <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="">
+//     <div class="card-body">
+//       <h5 class="card-title">Car ${i}</h5>
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//       <a href="producto.html" class="btn btn-primary">Ver más</a>
+//     </div>
+//   </div>`) ;
+//   }
+//   section.innerHTML = array.join(``).replaceAll(",", "");
+// }
+
+// homeDinamico()
--- producto.js
+++ producto.js
@@ -1,23 +1,130 @@
-class Producto {
-    constructor (titulo, detalle, precio, stock, imagen) {
-        this.titulo= titulo;
-        this.detalle= detalle;
-        this.precio= precio;
-        this.stock= stock;
-        this.imagen= imagen;
-    }
-}
+const main = document.querySelector("main");
 
-const auto = new Producto ("Bmw m240i", "BMW Serie 2 3.0 M240i F22 Coupe", "$3600", 10, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg")
-let etiquetas=  ` <div>
-<h1> Descripción </h1>
-<h2> Titulo: ${auto.titulo}</h2>
-<p> Detalle: ${auto.detalle}</p>
-<p> Precio: ${auto.precio}</p>
-<p> Stock: ${auto.stock}</p>
-<img src="${auto.imagen}" alt="Imagen del auto widht="80%" height="300px" align-items="center"/>
-</div>
-`
-let main= document.querySelector ("main");
-main.innerHTML= etiquetas;
+const elemento = window.location.search.split("=")[1];
 
+const autos = [
+    {
+    id: 1,
+    modelo: "Tundra",
+    descripcion: "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    precio: 34,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 2,
+    modelo: "I",
+    descripcion: "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
+    precio: 62,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 3,
+    modelo: "M3",
+    descripcion: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    precio: 14,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 4,
+    modelo: "Rio",
+    descripcion: "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    precio: 66,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 5,
+    modelo: "Ram Van B250",
+    descripcion: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+    precio: 99,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 6,
+    modelo: "Econoline E150",
+    descripcion: "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    precio: 23,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 7,
+    modelo: "Rabbit",
+    descripcion: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    precio: 54,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 8,
+    modelo: "928",
+    descripcion: "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
+    precio: 63,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }, {
+    id: 9,
+    modelo: "Intrepid",
+    descripcion: "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+    precio: 52,
+    url: `https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${this.id}.jpg`,
+  }];
+
+
+const carFind = autos.find((autos) => autos.id == elemento);
+
+let etiquetas = `<div class="producto-container">
+    <div class="producto-main">
+        <div class="left-section">
+            <img src="${carFind.url}" alt="Car ${carFind.id}" id="main-image">
+        </div>
+
+        <div class="center-section">
+            <div class="product-info">
+                <h1 class="producto-h1">${carFind.modelo}</h1>
+                <p class="description">${carFind.descripcion}</p>
+                <div class="rating">
+                    <span>⭐⭐⭐⭐⭐</span> <a href="#">(3)</a>
+                </div>
+                <p class="original-price">$100.000</p>
+                <p class="discounted-price">$${carFind.precio} <span class="discount">(5% OFF)</span></p>
+                <p class="installments">In 6 installments of $3,799.78</p>
+            </div>
+        </div>
+
+        <div class="right-section">
+            <div class="shipping-info">
+                <h3>Free Standard International Shipping.</h3>
+                <p>Estimated between Tue, Oct 22 and Wed, Nov 6.</p>
+                <p class="bold">${carFind.stock} available</p>
+                <div class="quantity-selector">
+                    <label for="quantity" class="quantity-label">Quantity:</label>
+                    <select id="quantity" class="quantity-dropdown">
+                        <option value="1">1 unit</option>
+                        <option value="2">2 units</option>
+                        <option value="3">3 units</option>
+                        <option value="4">4 units</option>
+                        <option value="5">5 units</option>
+                        <option value="6">6 units</option>
+                    </select>
+                </div>
+            <button class="btn primary-btn">Buy it now</button>
+            <button class="btn secondary-btn">Add to cart</button>
+        </div>
+    </div>
+</div>`;
+
+main.innerHTML = etiquetas;
+
+
+// class Producto {
+//     constructor (titulo, detalle, precio, stock, imagen) {
+//         this.titulo= titulo;
+//         this.detalle= detalle;
+//         this.precio= precio;
+//         this.stock= stock;
+//         this.imagen= imagen;
+//     }
+// }
+
+// const auto = new Producto ("Bmw m240i", "BMW Serie 2 3.0 M240i F22 Coupe", "$3600", 10, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg")
+// let etiquetas=  ` <div>
+// <h1> Descripción </h1>
+// <h2> Titulo: ${auto.titulo}</h2>
+// <p> Detalle: ${auto.detalle}</p>
+// <p> Precio: ${auto.precio}</p>
+// <p> Stock: ${auto.stock}</p>
+// <img src="${auto.imagen}" alt="Imagen del auto widht="80%" height="300px"/>
+// </div>
+// `
+// let main= document.querySelector ("main");
+// main.innerHTML= etiquetas;
